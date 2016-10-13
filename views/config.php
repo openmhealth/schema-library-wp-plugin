13539e5a6cae4050f9b01bab29462cc87ebc3188
Jasper Speicher <jasper.speicher@gmail.com>
2016-10-13 15:21:31 -0700

    Add version, improve layout with horizontal rules

--- views/config.php
+++ views/config.php
@@ -8,11 +8,14 @@
 namespace OMHSchemaLibrary;
 ?>
 
+<h1>Schema Library v 1.1</h1>
+<hr/>
 <h2>Configuration Options</h2>
 
 <?php view('options', $data ); ?>
 <br/>
 
+<hr/>
 <h2>Library Management</h2>
 
 <a  class="button button-primary update-button" href="http://<?php echo remove_qsvar( $data['plugin_full_url'], 'updateLibrary' ) . "&updateLibrary=true"; ?>">Update Library</a>
